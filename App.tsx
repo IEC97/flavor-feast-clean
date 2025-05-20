702eb2a8b9bb24d43a915ba9cfb91256c6cbb828
Luca Lamorte <llamorte@uade.edu.ar>
2025-05-19 21:09:42 -0300

    Pasado a limpio

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
+import { RecipeProvider } from './context/RecipeContext';
+import { FilterProvider } from './context/FilterContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RecipeProvider>
+      <FilterProvider>
+        <AppNavigator />
+      </FilterProvider>
+    </RecipeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
